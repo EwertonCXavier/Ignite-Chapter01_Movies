69e5ffca9a5f9c2dfb31d2b987d81704c2b7142f
EwertonCXavier <ewertoncassianox@gmail.com>
2021-10-02 10:50:45 -0400

    Finalizaçao do desafio extra

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+
+
+interface PropsContent {
+  movies: MovieProps[];
+}
+
+
+export function Content(props: PropsContent) {
+  console.log("Here we go!");
+  console.log(props);
+  return (
+    <main>
+      <div className="movies-list">
+        {props.movies.map(movie=> (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+interface PropsHeader {
+    title: string;
+}
+
+
+
+export function Header (props: PropsHeader){
+    return (
+        <header>
+          <span className="category">Categoria:<span> {props.title}</span></span>
+        </header>
+    )
+}
